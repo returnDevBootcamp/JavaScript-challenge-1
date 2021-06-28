3a73268e45594bc36bc0aeaa8e4f6dba755a6234
Sara-Issa <85680247+Sara-Issa@users.noreply.github.com>
2021-06-28 19:39:21 +0300

    done

--- Part-1/challenge.js
+++ Part-1/challenge.js
@@ -1,2 +1,29 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+var mohamedweight = 78;
+var mohamedheight = 1.69;
+
+var aliweight = 92;
+var aliheight = 1.95;
+
+var mohamedBMI = mohamedweight / mohamedheight ** 2;
+console.log(mohamedBMI);
+
+var aliBMI = aliweight / aliheight ** 2;
+console.log(aliBMI);
+
+console.log(`mohamed BMI is > then ali BMI `);
+console.log(mohamedBMI > aliBMI);
+
+var Mohamedweight = 95;
+var Mohamedheight = 1.88;
+
+var Aliweight = 85;
+var Aliheight = 1.79;
+
+var MohamedBMI = Mohamedweight / Mohamedheight ** 2;
+console.log(MohamedBMI);
+
+var AliBMI = Aliweight / Aliheight ** 2;
+console.log(AliBMI);
+
+console.log(`Mohamed BMI is > then Ali BMI `);
+console.log(MohamedBMI > AliBMI);
